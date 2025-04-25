acd65de3806ba20801279898dc7cadd565823722
sidd-a1421 <achintasiddhanta0304@gmail.com>
2025-04-25 16:28:43 +0530

    ff

--- script.js
+++ script.js
@@ -16,10 +16,17 @@ apifetch();
 
 const btn=document.querySelector('.submit-btn');
 const output=document.querySelector('.output')
+const input=document.querySelector('.input')
 
 let html='';
 btn.addEventListener('click',()=>{    
+    if(input.value!=''){
     html+=`<p>Hamse to na ho payega :)</p>`;
     output.innerHTML=html;
+    }
+    else{
+        output.innerHTML=`Enter something`
+        output.classList.add('error')
+    }
 })
 
